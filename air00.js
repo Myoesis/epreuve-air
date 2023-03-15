802cfa1668d00b38c2fe7be1219879867a739d98
Myoesis <thomas.barreau1@gmail.com>
2023-03-15 13:06:18 +0100

    changement de la fonction pour pouvoir être appelé

--- air00.js
+++ air00.js
@@ -6,23 +6,16 @@
 
 
 //Déclarer mes variables :
-let argACouper = process.argv[2]
-let excedent = process.argv[3]
-let finalArray=[]
 
-
-// gestions des erreurs :
-let error = (arg1,arg2) => {
-    if(arg1===undefined || arg2) {
-        console.log("error")
-        process.exit()
+let error = (args) => {
+    if(args.length!=1) {
+        return "error"
     }
 }
 
-
-// mes fonctions : 
-let fonction_split = (aSpliter , separateur) => {            // le séparateur ici ne sert a rien??? consigne confuse ? 
-    separateur = /[\s,\t,\r]/ ;                              // regex pour séparateur, et non pas un argument.
+let fonction_split = (aSpliter) => {                             // le séparateur ici ne sert a rien??? consigne confuse ? 
+    let finalArray=[]
+    let separateur = /[\s,\t,\r]/ ;                              // regex pour séparateur, et non pas un argument.
     let indexRepere =0
     for (let i=0 ; i < aSpliter.length ; i++) {
         if (separateur.test(aSpliter[i])) {                      // si un élément passe par le regex (donc espace, retour a la ligne, tabulation)
@@ -30,13 +23,30 @@ let fonction_split = (aSpliter , separateur) => {            // le séparateur i
             indexRepere=i+1
         }
     }
-    if(indexRepere < aSpliter.length) {
+    if(indexRepere < aSpliter.length) {                      
         finalArray.push(aSpliter.slice(indexRepere))
     }
-    console.log(finalArray)
+    return finalArray
 }
+
+let maFonctionSplit =(arguments) => {
+
+error(arguments)
+return fonction_split(arguments)
+ 
+}    
 
 
 // appelle des fonctions : 
-error(argACouper,excedent)
-fonction_split(argACouper)
+if (require.main === module) {
+    const arguments = process.argv.slice(2)
+    if (error(arguments) === "error") {
+        console.log("error")
+        process.exit()
+    }
+    console.log(maFonctionSplit(arguments[0]))
+} else {
+    module.exports ={maFonctionSplit
+                }
+}
+
